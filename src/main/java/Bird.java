6f63ddd0244ce1bd9e28ec4e1d85343ad1c126ff
Tinuola Ogunniki <ogunnoikiace@yahoo.com>
2023-03-03 23:34:52 +0000

    Completed

--- src/main/java/Bird.java
+++ src/main/java/Bird.java
@@ -31,7 +31,11 @@ public class Bird {
     /**
      * A variable (in this context, often referred to as a field), which represents the age of the bird.
      */
+
+    
+
     public int ageMonths;
+
 
     /**
      * A constructor for the bird class: this method allows us to build a new bird with the 'new' keyword.
@@ -39,13 +43,18 @@ public class Bird {
      */
     public Bird(){
 
+
     }
     /**
      * @return "Tweet" if ageMonths is less than 3 and "Chirp" if ageMonths is
      *         greater than or equal to 3.
      */
     public String sound(){
-        return "";
+        if (ageMonths < 3){
+            return "Tweet";
+        }else if (ageMonths >= 3){
+        }
+        return "Chirp";
     }
 
 }
